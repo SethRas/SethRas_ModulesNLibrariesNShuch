609d6468297ba7a12f9c89c14d3406f3b438fa03
rasset69@gmail.com <rasset69@gmail.com>
2022-02-24 08:58:58 -0700

    Add project files.

---
+++ SethRas_ModulesNLibrariesNShuch/MyAwesomeApp.vb
@@ -0,0 +1,22 @@
+﻿Module MyAwesomeApp
+
+
+    Sub Main()
+        Dim temp As String
+        Dim menu As String
+
+        'menu = "1. Delete" & vbNewLine _ 'space underscore indicates to software that the instruction is the same just on a new line
+        '    & "2. Save" & vbNewLine _
+        '    & "3. Rename" & vbNewLine
+
+        'temp = (MyCoolLbrary.Prompt(menu))
+
+        Prompt()
+
+        'Console.WriteLine(temp)
+        Console.Read()
+
+
+    End Sub
+
+End Module
---
+++ SethRas_ModulesNLibrariesNShuch/MyCoolLbrary.vb
@@ -0,0 +1,27 @@
+﻿Module MyCoolLbrary
+
+
+    'Public Function SayJello()
+    '    Return "Jello"
+    'End Function
+
+
+    ''' <summary> 
+    ''' Displays the contes of arguments displayMessage the returns the user response. 
+    ''' <returns> string: user response
+    '''
+
+    Public Function Prompt(ByVal Optional displayMessage As String = "<empty>") As String
+
+        Console.WriteLine(displayMessage)
+        Return Console.ReadLine()
+
+    End Function
+
+
+    Public Function randomWithinRange(max As Integer, min As Integer) As Integer
+        max = max - min
+        Randomize(DateTime.Now.Millisecond)
+        Return CInt(Rnd() * max) + min
+    End Function
+End Module
